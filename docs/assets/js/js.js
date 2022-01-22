9532b28c7af9d5e9a8e687856418e0f9ec4be6fa
Steffen Brandtzæg <steffen.brandtzaeg@avento.no>
2022-01-22 10:47:34 +0100

    Kapittelliste v2

--- docs/assets/js/js.js
+++ docs/assets/js/js.js
@@ -3,10 +3,10 @@ window.onload = chapterList;
 function chapterList() {
     const chapterNav = document.getElementById('chapternav');
     const chapters = [...document.querySelectorAll('h2')];
-    chapters.forEach(element => makeChapterLink(element));
+    chapters.forEach(element => makeChapterLink(element, chapterNav));
 }
 
-function makeChapterLink(element) {
+function makeChapterLink(element,chapterNav) {
     let item = document.createElement("a");
     item.innerHTML = element.innerHTML;
     chapterNav.appendChild(item);
